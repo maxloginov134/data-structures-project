b7a4468fce1a894418909690181cce6a81f8d704
maxlo <maxloginov2001@mail.ru>
2023-03-14 14:53:59 +0300

    commit stack.py and test.stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,8 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+"""Здесь надо написать тесты с использованием pytest для модуля stack."""
+from src.stack import Node, Stack
+
+
+def test_node_init():
+    assert Node(next_node="next_node", data="data1")
+
+
