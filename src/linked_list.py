6752c1ee5c734fa8717174538d4f5c284073d112
maxlo <maxloginov2001@mail.ru>
2023-03-30 20:36:40 +0300

    commit linked_list.py and test_linked_list.py

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,36 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self, head=None, end=None):
+        self.head = head
+        self.end = end
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        beginning_node = Node(data)
+
+        if self.head is None:
+            self.end = beginning_node
+        else:
+            beginning_node.next_node = self.head
+
+        self.head = beginning_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        end_node = Node(data)
+        if self.head is None:
+            self.head = end_node
+        else:
+            self.end.next_node = end_node
+        self.end = end_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,53 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import LinkedList, Node
+
+
+class TestNodeCls(unittest.TestCase):
+    def test_init_node(self):
+        test_node = Node()
+        self.assertIsNone(test_node.data)
+        self.assertIsNone(test_node.next_node)
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_init_node(self):
+        test_linked_list = LinkedList()
+        self.assertIsNone(test_linked_list.head)
+        self.assertIsNone(test_linked_list.end)
+
+    def test_first_insert_to_begin_ll(self):
+        test_linked_list = LinkedList()
+        test_linked_list.insert_beginning({'id': 1})
+        self.assertEqual(test_linked_list.head.data, {'id': 1})
+        self.assertEqual(test_linked_list.end.data, {'id': 1})
+
+    def test_insert_to_begin_ll_twice(self):
+        test_linked_list = LinkedList()
+        test_linked_list.insert_beginning({'id': 1})
+        test_linked_list.insert_beginning({'id': 2})
+        self.assertEqual(test_linked_list.head.data, {'id': 2})
+        self.assertEqual(test_linked_list.end.data, {'id': 1})
+
+    def test_insert_to_end_ll(self):
+        test_linked_list = LinkedList()
+        test_linked_list.insert_at_end({'id': 2})
+        self.assertEqual(test_linked_list.end.data, {'id': 2})
+
+    def test_insert_to_end_ll_twice(self):
+        test_linked_list = LinkedList()
+        test_linked_list.insert_beginning({'id': 1})
+        test_linked_list.insert_at_end({'id': 2})
+        self.assertEqual(test_linked_list.end.data, {'id': 2})
+        self.assertEqual(test_linked_list.head.data, {'id': 1})
+
+    def test_empty_str(self):
+        test_linked_list = LinkedList()
+        self.assertIsNone(print(test_linked_list))
+
+    def test_filled_ll(self):
+        test_linked_list = LinkedList()
+        test_linked_list.insert_beginning({'id': 1})
+        test_linked_list.insert_at_end({'id': 2})
+        self.assertEqual(str(test_linked_list), " {'id': 1} -> {'id': 2} -> None")
