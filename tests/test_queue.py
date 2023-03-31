77e01b0b73558e7582a2e189eb3c58b7e5400856
maxlo <maxloginov2001@mail.ru>
2023-03-31 22:17:31 +0300

    commit test_queue.py fix Error

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -10,7 +10,7 @@ class TestStak(unittest.TestCase):
         test_queue.enqueue('data1')
         test_queue.enqueue('data2')
         self.assertEqual(test_queue.head.data, 'data1')
-        self.assertEqual(test_queue.head.next_node, 'data2')
+        self.assertEqual(test_queue.head.next_node.data, 'data2')
 
     def test_dequeue(self):
         test_queue = queue.Queue()
@@ -24,5 +24,5 @@ class TestStak(unittest.TestCase):
         test_queue = queue.Queue()
         test_queue.enqueue('data1')
         test_queue.enqueue('data2')
-        self.assertEqual(str(test_queue), "data2\ndata2")
+        self.assertEqual(str(test_queue), "data1\ndata2")
 
